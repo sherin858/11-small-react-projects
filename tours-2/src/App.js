5632d7a455a4efb21c6be4e46b01a8e55c7b13e1
sherin858 <sherin.reda15@gmail.com>
2022-03-24 18:53:00 -0700

    tours

--- tours-2/src/App.js
+++ tours-2/src/App.js
@@ -4,18 +4,35 @@ import { useEffect,useState } from 'react';
 import axios from 'axios';
 function App() {
   const [tours,setTours]=useState([]);
-  const url='https://course-api.com/react-tours-project';
-  useEffect(()=>{
+  const removeTour=(id)=>{
+    const updatedTours=tours.filter((tour)=>tour.id!==id);
+    setTours(updatedTours);
+  }
+  const getTours=()=>{
     axios.get('https://course-api.com/react-tours-project')
     .then(res=>setTours(res.data))
     .catch(err=>console.log(err))
+  }
+  useEffect(()=>{
+    getTours()
   },[])
+  const refresh=()=>{
+    getTours();
+  }
   return (
-    <div className="App justify-content-center bg-light">
+    <div className="App justify-content-center">
       <div className="container d-flex flex-column align-items-center">
         <h2>Our Tours</h2>
         <div className="line mb-5"></div>
-        <Card tours={tours}/>
+        {
+          tours.length?
+          <Card tours={tours} removeTour={removeTour} />:
+          <div>
+              <h5>No Tours Left</h5>
+              <button onClick={()=>refresh()} type="button" className='btn' style={{color:'hsl(205, 78%, 60%)' ,outline:'hsl(205, 78%, 60%) solid 0.01rem'}} >Refresh</button>
+          </div>
+        }
+        
       </div>
     </div>
   );
--- tours-2/src/components/Card.js
+++ tours-2/src/components/Card.js
@@ -1,23 +1,35 @@
 import React from 'react'
-
-function Card({tours}) {
+import {useState} from 'react'
+function Card({tours,removeTour}) {
+  const [fullText,setFullText]=useState(false);
     return(
         <>
-                {
+          {
             tours.map((tour)=>{
             return(
             <div className='row  justify-content-center mb-5' key={tour.id} >
             <div className="col-lg-6 tour">
                 <img className='mb-3' src={tour.image} alt={tour.name} />
-                <div>
-                    <h4>{tour.name}</h4>
-                    <p>{tour.info}</p>
+                <div className='p-4'>
+                    <div className='d-flex justify-content-between'>
+                      <h4>{tour.name}</h4>
+                      <h4 className='price'>${tour.price}</h4>
+                    </div>
+                      {
+                      fullText?
+                          <p>{tour.info}<button onClick={()=>setFullText(!fullText)} type="button" className='btn btn-link shadow-none p-0 border-0'>...show less</button>
+                          </p>:
+                          <p>
+                            {tour.info.substr(0,200)}<button onClick={()=>setFullText(!fullText)} type="button" className='btn btn-link shadow-none p-0 border-0'>...show more</button>
+                          </p>
+                      }
+                      <button onClick={()=>removeTour(tour.id)} type='button' className='btn btn-outline-danger'style={{margin:"0 auto", display:'block',width:'auto'}} >Not Interested</button>
                 </div>
               </div>
             </div>
-          )
-        })
-      }
+            )
+            })
+          }
         </>
     )
 
